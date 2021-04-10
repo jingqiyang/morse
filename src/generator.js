ac028694474681a79ee4d8b29c644dd051210ce5
jingqiyang <jyang1998@gmail.com>
2021-04-10 09:47:41 -0400

    added enter key + result message

--- src/generator.js
+++ src/generator.js
@@ -13,30 +13,75 @@ var beepDelay = 0.075
 var initialized = false;
 var ctx = null;
 var dot = null, dash = null;
+var hiddenHint = true;
 var randWord = null;
 var morseWord = null;
 
 var playbutton = document.getElementById("playbutton");
 var stopbutton = document.getElementById("stopbutton");
-var container = document.getElementById("container");
 var textbox = document.getElementById("textbox");
+var hint = document.getElementById("hint");
+var result = document.getElementById("result");
 
 
 /* * * * * buttons * * * * */
 
-// on play button click, play/replay current word
-playbutton.onclick = async () => {
+// on play button click, play current word
+playbutton.onclick = playCurrWord;
+
+
+// on enter key, play current word
+textbox.addEventListener("keyup", async function(event) {
+    if (event.keyCode === 13) {
+        playCurrWord();
+    }
+});
+
+// start or go to next word, or replay current word
+async function playCurrWord() {
     if (!initialized) {
         await initAudio().then(async () => {
             initialized = true;
-            generateWord();
-            await playMorseAudio(morseWord);
+            hint.innerHTML = "click to show/hide hint";
+            generateNext();
         })
-    }
-    else {
+    } else if (correct) {
+        correct = false;
+        textbox.value = "";
+
+        hiddenHint = true;
+        hint.innerHTML = "click to show/hide hint";
+
+        // fade out success message
+        setTimeout(function() {
+            var timerId = setInterval(function() {
+                let opacity = result.style.opacity;
+                if (opacity == 0) {
+                    clearInterval(timerId);
+                } else {
+                    result.style.opacity = opacity - 0.05;
+                }
+            }, 50);
+        }, 100);
+
+        generateNext();
+    } else {
         await playMorseAudio(morseWord);
     }
-};
+}
+
+// show/hide hint
+window.onload = function() {
+    hint.onclick = function () {
+        if (hiddenHint) {
+            hiddenHint = false;
+            hint.innerHTML = morseWord;
+        } else {
+            hiddenHint = true;
+            hint.innerHTML = "click to show/hide hint";
+        }
+    };
+}
 
 // on stop button click, stop current word
 stopbutton.onclick = async () => {
@@ -101,29 +146,41 @@ function createMorseBuffer(str) {
 
 /* * * * * word generation and matching * * * * */
 
-// generate new random word
-function generateWord() {
-    randWord = getRandomWord();
-    morseWord = toMorseText(randWord);
-    console.log(randWord + "\n" + morseWord);
-    // container.innerHTML = randWord + "\n" + morseWord;
-}
-
 // when text box input matches, genenerate new word
 textbox.addEventListener('input', async () => {
     let response = textbox.value;
 
     if (randWord == response) {
-        textbox.value = "";
         if (ctx) {
             await ctx.close().then(async () => {
                 ctx = null;
-                generateWord();
-                await playMorseAudio(morseWord);
+                handleSuccess();
+                
             });
         } else {
-            generateWord();
-            await playMorseAudio(morseWord);
+            handleSuccess();
         } 
     }
 });
+
+var correct = false;
+
+// set response when entered correct answer
+function handleSuccess() {
+    correct = true;
+    result.style.opacity = 1;
+    result.innerHTML = "correct!<br />" + randWord;
+}
+
+// start next round
+async function generateNext() {
+    generateWord();
+    await playMorseAudio(morseWord);
+}
+
+// generate new random word
+function generateWord() {
+    randWord = getRandomWord();
+    morseWord = toMorseText(randWord);
+    console.log(randWord + "\n" + morseWord);
+}
